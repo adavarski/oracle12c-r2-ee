e97d984e7d5e056accfe1914e02ae4a998e905f1
adavarski <42774466+adavarski@users.noreply.github.com>
2019-02-05 05:36:34 +0200

    Update createDB.sh

--- createDB.sh
+++ createDB.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-# LICENSE CDDL 1.0 + GPL 2.0
-#
-# Copyright (c) 1982-2016 Oracle and/or its affiliates. All rights reserved.
-#
-# Since: November, 2016
-# Author: gerald.venzl@oracle.com
-# Description: Creates an Oracle Database based on following parameters:
-#              $ORACLE_SID: The Oracle SID and CDB name
-#              $ORACLE_PDB: The PDB name
-#              $ORACLE_PWD: The Oracle password
-#
-# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
-#
-# Modified: steve.swinsburg@gmail.com
-#           Added more configuration options
 
 set -e
 
