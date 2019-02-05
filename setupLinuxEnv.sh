d5309b3d28d39986bc78e9f8799236066bd2382e
adavarski <42774466+adavarski@users.noreply.github.com>
2019-02-05 05:35:01 +0200

    Update setupLinuxEnv.sh

--- setupLinuxEnv.sh
+++ setupLinuxEnv.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
-# LICENSE CDDL 1.0 + GPL 2.0
-#
-# Copyright (c) 1982-2016 Oracle and/or its affiliates. All rights reserved.
-#
-# Since: December, 2016
-# Author: gerald.venzl@oracle.com
-# Description: Sets up the unix environment for DB installation.
-# 
-# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
-# 
 
-# Setup filesystem and oracle user
-# Adjust file permissions, go to /opt/oracle as user 'oracle' to proceed with Oracle installation
 # ------------------------------------------------------------
 mkdir -p $ORACLE_BASE/scripts/setup && \
 mkdir $ORACLE_BASE/scripts/startup && \
