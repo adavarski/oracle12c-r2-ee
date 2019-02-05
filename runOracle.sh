6efbf9477c847876cb77e5ead316f911c210ae61
adavarski <42774466+adavarski@users.noreply.github.com>
2019-02-05 05:37:42 +0200

    Update runOracle.sh

--- runOracle.sh
+++ runOracle.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-# LICENSE CDDL 1.0 + GPL 2.0
-#
-# Copyright (c) 1982-2016 Oracle and/or its affiliates. All rights reserved.
-# 
-# Since: November, 2016
-# Author: gerald.venzl@oracle.com
-# Description: Runs the Oracle Database inside the container
-# 
-# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
-# 
 
 ########### Move DB files ############
 function moveFiles {
